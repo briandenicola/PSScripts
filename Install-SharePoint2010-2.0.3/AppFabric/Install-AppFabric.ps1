4de2a116df55980f5b27bf965c8c723bdee0b21d
Brian Denicola <bjd145@yahoo.com>
2012-05-18 21:28:31 -0500

    AppFabric Install

--- Install-SharePoint2010-2.0.3/AppFabric/Install-AppFabric.ps1
+++ Install-SharePoint2010-2.0.3/AppFabric/Install-AppFabric.ps1
@@ -1,10 +1,19 @@
-copy \\ent-nas-fs01.us.gt.com\app-ops\Installs\WindowsServerAppFabricSetup_x64_6.1.exe D:\Deploy\
-powershell.exe -ImportSystemModules -command { Add-WindowsFeature  AS-Web-Support;  Add-WindowsFeature  AS-HTTP-Activation }
+ImportSystemModules
+Add-WindowsFeature  AS-Web-Support
+Add-WindowsFeature  AS-HTTP-Activation 
 
-D:\Deploy\WindowsServerAppFabricSetup_x64_6.1.exe /install HostingServices
+$source = "http://download.microsoft.com/download/1/A/D/1ADC8F3E-4446-4D31-9B2B-9B4578934A22/WindowsServerAppFabricSetup_x64_6.1.exe"
+$dest = "D:\Temp"
+$exe = "WindowsServerAppFabricSetup_x64_6.1.exe"
 
-while( (Get-Process | where { $_.ProcessName -eq "WindowsServerAppFabricSetup_x64_6.1" }) -ne $nul)
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile( $source, (Join-Path $dest $exe) )
+
+&(Join-Path $dest $exe) /install HostingServices
+
+while( (Get-Process | where { $_.ProcessName -eq $exe }))
 { 
 	Write-Host -NoNewline "."
 	Sleep 5
 }
+Write-Host "`n Install Complete"
