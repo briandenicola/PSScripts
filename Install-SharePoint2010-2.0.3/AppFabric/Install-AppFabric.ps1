d91a7d63928872493036027087e9699aa3c27b8e
Brian Denicola <bjd145@yahoo.com>
2012-05-19 08:27:24 -0500

    Fixed logging in Install-AppFabric

--- Install-SharePoint2010-2.0.3/AppFabric/Install-AppFabric.ps1
+++ Install-SharePoint2010-2.0.3/AppFabric/Install-AppFabric.ps1
@@ -3,6 +3,11 @@ param(
 	[string] $module = "HostingServices"
 )
 
+function log( [string] $txt ) 
+{
+	"[" + (Get-Date).ToString() + "] - " + $txt 
+}
+
 ImportSystemModules
 Add-WindowsFeature  AS-Web-Support
 Add-WindowsFeature  AS-HTTP-Activation 
@@ -11,11 +16,16 @@ $source = "http://download.microsoft.com/download/1/A/D/1ADC8F3E-4446-4D31-9B2B-
 $dest = "D:\Temp"
 $exe = "WindowsServerAppFabricSetup_x64_6.1.exe"
 
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile( $source, (Join-Path $dest $exe) )
+if( -not ( Test-Path (Join-Path $dest $exe) ) )
+{
+	log -txt "Downloading $source"
+	$wc = New-Object System.Net.WebClient
+	$wc.DownloadFile( $source, (Join-Path $dest $exe) )
+}
 
 if( $module -eq "complete" ) { $module = "HostingServices,CachingService,CacheClient" }
 
+log -txt "Installing $exe with $module modules"
 &(Join-Path $dest $exe) /install $module
 
 while( (Get-Process | where { $_.ProcessName -eq $exe }))
@@ -23,4 +33,4 @@ while( (Get-Process | where { $_.ProcessName -eq $exe }))
 	Write-Host -NoNewline "."
 	Sleep 5
 }
-Write-Host "`n Install Complete"
+log -txt "`n Install Complete"
