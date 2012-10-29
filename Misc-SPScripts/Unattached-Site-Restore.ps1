df3fd7b77063a0ac52a4902c84db626973ca3e13
Brian Denicola <bjd145@yahoo.com>
2012-10-29 09:59:49 -0500

    Script Rename

--- Misc-SPScripts/Unattached-Site-Restore.ps1
+++ Misc-SPScripts/Unattached-Site-Restore.ps1
@@ -1,7 +1,8 @@
 param ( 
 	[string] $app,
 	[string] $site,
-	[string] $db_name
+	[string] $db_name,
+	[switch] $restore
 )
 
 Add-PSSnapin Microsoft.SharePoint.Powershell
@@ -9,17 +10,24 @@ Add-PSSnapin Microsoft.SharePoint.Powershell
 $db_server = "sql"
 $db = Get-SPContentDatabase -ConnectAsUnattachedDatabase -DatabaseName $db_name -DatabaseServer $db_server
 	
-$restored_site = $db | Get-SPSite -Limit All | Where ServerRelativeUrl -match $site
-$restored_file = Join-Path $ENV:TEMP ("temp-restore-" + $(Get-Date).ToString("yyyyMMddhhmmss") + ".bak")
+$restored_site = $db | Get-SPSite -Limit All | Where ServerRelativeUrl -imatch $site
+$restored_file = Join-Path $ENV:TEMP ($site + "-backup-" + $(Get-Date).ToString("yyyyMMddhhmmss") + ".bak")
 
-if( -not $restored_site ) {
-	Backup-SPSite -Identity $restored_site -FilePath $restored_file
+if( $restored_site -ne $null ) {
+	Backup-SPSite -Identity $restored_site -Path $restored_file -Verbose
 
-	if( -not (Get-SPSite ($app + "/" + $site) -Limit All ) ) { 
-		Restore-SPSite ($app + "/" + $site) -File $restored_file -Verbose
+	if( $restore ) {
+		$recover_url = $app + $restored_site.ServerRelativeUrl
+		$org_site = Get-SPSite $recover_url -Limit All -EA SilentlyContinue
+		if( $org_site -eq $null ) { 
+			Restore-SPSite $recover_url -Path $restored_file -Verbose
+		}
+		else { 
+			Write-Error "The site - $site - was already found in $app. Will not restore automatically. Backup of file can be found at - $restored_file"
+		}
 	}
 	else { 
-		Write-Error "The site - $site - was already found in $app. Will not restore automatically. Backup of file can be found at - $restored_file"
+		Write-Host "The site - $site - has been backed up to -  $restored_file"
 	}
 }
 else { 
