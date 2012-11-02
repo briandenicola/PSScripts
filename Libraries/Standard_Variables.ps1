1aabdba780e269a0d310609dbbac7f4306165020
Brian Denicola <bjd145@yahoo.com>
2012-11-02 14:50:51 -0500

    Updates for Audit

---
+++ Libraries/Standard_Variables.ps1
@@ -0,0 +1,7 @@
+$global:SharePoint_url = "http://teamadmin.gt.com/sites/ApplicationOperations/"
+$global:SharePoint_sql_server_list = "SQL Servers"
+$global:SharePoint_sql_server_all_items_view = '{CC71240E-557E-4851-B953-7B9757DCAEF0}'
+	
+$global:AppOps_url = "http://teamadmin.gt.com/sites/ApplicationOperations/applicationsupport/"
+$global:AppOps_sql_server_list = "SQL Servers"
+$global:AppOps_sql_server_all_items_view = '{400E0A67-F9E9-42D5-BD03-4E29F06503CF}'
