6a032417e5d64fbc76ba0a73e93bf3558fdb246a
Brian Denicola <bjd145@yahoo.com>
2012-11-28 12:51:26 -0600

    Updates to Master Deploy

--- Install-DotNet-WebFarm/Master-Setup-DotNet-Server.ps1
+++ Install-DotNet-WebFarm/Master-Setup-DotNet-Server.ps1
@@ -11,18 +11,19 @@ param(
 
     [string] $farm_name = [String]::Empty,
     [string] $service_account = [String]::Empty,
-	[string] $controller = [String]::Empty,
+	
 	[switch] $record
 )
 
 . .\Libraries\BootStrap_Functions.ps1
 
+
 #Set Variables
 $cfg = [xml] ( gc ".\Config\web_server_setup.xml")
 
-$source = $cfg.parameters.source 
-$gac_source = $cfg.parameters.gac
-$url = $cfg.parameters.url 
+$source = $cfg.parameters.source #"\\dc01\app-ops\Tools"
+$gac_source = $cfg.parameters.gac #"\\dc01\app-ops\Code"
+$url = $cfg.parameters.url #"http://teamadmin.gt.com/sites/ApplicationOperations/applicationsupport/"
 $scripts_home = $cfg.parameters.scripts
 $utils_home = $cfg.parameters.utils
 $webpi = $cfg.parameters.webpi
@@ -65,6 +66,7 @@ function Setup-DotNet
 	&$webpi /Install /Products:MVC3 /accepteula /SuppressReboot 
 	&$webpi /Install /Products:NETFramework4 /accepteula /SuppressReboot 
 	&$webpi /Install /Products:ARR /accepteula /SuppressReboot 
+    &$webpi /Install /Products:WDeplolyNoSMO /accepteula /SuppressReboot 
 	&$webpi /List /ListOption:Installed
 	C:\Windows\Microsoft.NET\Framework64\v4.0.30319\aspnet_regiis.exe -iru
 }
@@ -104,47 +106,47 @@ function Install-WebFarm-Software
 
 function Create-Farm
 {
-	Add-ServiceAccount
-	Install-WebFarm-Software
-
 	. (Join-Path $ENV:SCRIPTS_HOME "Libraries\IIS_Functions.ps1") 
+
+    Add-WindowsFeature Web-Mgmt-Service
+	Add-ServiceAccount
 	$creds = Get-Credential $service_account
-	
+	Install-WebFarm-Software
 	Write-Host "[ $(Get-Date) ] - Creating Farm - $farm_name . . ."
 	Create-WebFarm -name $farm_name -primary $ENV:COMPUTERNAME -creds $creds
-	Start-Service WebFarmService
+
+    Start-Service WebFarmService
+    Start-Service MsDepSvc
+    Start-Service WebFarmAgentService
+
+    Set-Service -StartupType Automatic -Name WebFarmService
+    Set-Service -StartupType Automatic -Name MsDepSvc
+    Set-Service -StartupType Automatic -Name WebFarmAgentService
 }
 
 function Join-WebFarm
 {
-	Add-ServiceAccount
-	Install-WebFarm-Software
+    . (Join-Path $ENV:SCRIPTS_HOME "Libraries\IIS_Functions.ps1") 
 	
-	$sb = {
-		param (
-			[string] $farm,
-			[string] $sever
-		)
-		. (Join-Path $ENV:SCRIPTS_HOME "Libraries\IIS_Functions.ps1") 
-		
-		if( (Get-WebFarmServers -name $farm) -notcontains $server ) {
-		
-			Write-Host "[ $(Get-Date) ] - Joining farm - $farm  . . ."
-			Add-ServersToWebFarm -name $farm -members $server
-			Sync-WebFarm -name $farm
-		}
-		else {
-			Write-Host "$server is already a member of $farm_name . . ."
-		}
-	}
-	
-	Invoke-Command -Computer $controller -ScriptBlock $sb -ArgumentList $farm_name, $ENV:COMPUTERNAME
+    if( (Get-WebFarmServers -name $farm_name) -notcontains $ENV:COMPUTERNAME ) {
+        Write-Host "[ $(Get-Date) ] - Joining farm - $farm_name  . . ."
+		Add-ServiceAccount
+	    Install-WebFarm-Software
+	    Add-ServersToWebFarm -name $farm_name -members $ENV:COMPUTERNAME
+        Sync-WebFarm -name $farm_name
+    }
+    else {
+        Write-Host "$($ENV:COMPUTERNAME) is already a member of $farm_name . . ."
+    }
+
+    Start-Service WebFarmAgentService
+    Set-Service -StartupType Automatic -Name WebFarmAgentService
 }
 
 function main
 {	
 	Set-ExecutionPolicy unrestricted -force
-	if( -not (Test-Path $logs_home) ) { 
+	if( !(Test-Path $logs_home) ) { 
 		mkdir $logs_home
 		mkdir (Join-Path $logs_home "Trace")
 		net share Logs=$logs_home /Grant:Everyone,Read
@@ -157,10 +159,6 @@ function main
 		if( [String]::IsNullorEmpty($service_account) ) {
 			throw "Can not join $ENV:COMPUTERNAME to $farm_name without a service account . . . "	
 		}
-	}
-	
-	if( $farm -eq "join" -and [String]::IsNullorEmpty($controller) ) {
-		throw "Can not join $ENV:COMPUTERNAME to farm without a controller . . . "	
 	}
 	
 	$log = ".\Logs\System-Setup-" + $ENV:COMPUTERNAME + "-" + $(Get-Date).ToString("yyyyMMddhhmmss") + ".log"
