c0519d499b755b36bd1dc8722f2fa4c4ec46a1d0
Brian Denicola <bjd145@yahoo.com>
2012-12-26 19:14:02 -0600

    Version 1.0.2 of the SharePoint 2013 Scripts

---
+++ Install-SharePoint2010/KickStart-Install-SharePoint2010.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set DST=D:\SCRIPTS\Install-SharePoint2010\
+set SRC=\\ent-nas-fs01\app-ops\Installs\SharePoint\Install-SharePoint2010
+
+xcopy /e/v/f/s %SRC% %DST%
+
+echo Next Steps . . .
+echo 1) Edit .\configs\master_setup.xml appropriate for the environment
+echo 2) Run .\1_SPFarm-Master_Install.ps1
+
+powershell.exe -NoExit -NoProfile -Command "&{ param ( [string] $directory ); Set-ExecutionPolicy unrestricted; Start-Process Powershell.exe -Verb RunAs -WorkingDirectory $directory;}" %DST%
