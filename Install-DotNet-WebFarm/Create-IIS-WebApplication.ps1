7a61a6b0fc06df7b226e463cc4f937cd7464fa9c
Brian Denicola <bjd145@yahoo.com>
2013-02-25 15:15:53 -0600

    New Scripts for SP2013

--- Install-DotNet-WebFarm/Create-IIS-WebApplication.ps1
+++ Install-DotNet-WebFarm/Create-IIS-WebApplication.ps1
@@ -1,7 +1,8 @@
 param (	
 	[Parameter(Mandatory=$true)]
 	[string] $config = ".\config.xml",
-	
+	[switch] $nofarm,
+	[switch] $record,
 	[string] $log = ".\dotnet_webfarm_site_creation.log"
 )
 
@@ -28,31 +29,30 @@ function main
 	$farm = $cfg.IISSites.WebFarm.Name
 	$env = $cfg.IISSites.WebFarm.Environment
 	
-	$webFarm = Get-WebFarm -WebFarm $farm
-	if( $webFarm -eq $null )
-	{
-	   throw ( "Could not find the WebFarm " + $farm )	
+	if( $nofarm ) {
+		$servers = @($ENV:COMPUTERNAME)
+	} 
+	else {
+		$webFarm = Get-WebFarm -WebFarm $farm
+		if( $webFarm -eq $null ) {
+			throw ( "Could not find the WebFarm " + $farm )	
+		}
+		$servers = Get-WebFarmServers -name $farm
 	}
-	$servers = Get-WebFarmServers -name $farm
 	
-	try
-	{
-		foreach( $site in $cfg.IISSites.Site )
-		{	
-			if( Test-Path ("IIS:\Sites\" + $site.Name) )
-			{
+	try	{
+		foreach( $site in $cfg.IISSites.Site )	{	
+			if( Test-Path ("IIS:\Sites\" + $site.Name) ) {
                 log -txt ( $site.Name + " already exists on this web farm" )
                 continue
             }
             
-			if( -not (Test-Path ("IIS:\AppPools\" + $site.AppPool.Name)) )
-			{	
+			if( -not (Test-Path ("IIS:\AppPools\" + $site.AppPool.Name)) ) {	
 				log -txt ( "Creating AppPool " + $site.AppPool.Name + " with user " + $site.AppPool.User )
 				Create-IISAppPool -apppool $site.AppPool.Name -user $site.AppPool.User -pass $site.AppPool.Pass -version $site.AppPool.Version				
 			}
 				
-			if (-not (Test-Path $site.path))
-			{
+			if (-not (Test-Path $site.path)) {
 				log -txt ( "Creating Directory for " + $site.Name + " at path " + $site.Path )
 				mkdir $site.path
 			}
@@ -65,21 +65,21 @@ function main
 			log -txt ( "Setting Site " + $site.Name + " to use AppPool " + $site.AppPool.Name )
 			Set-IISAppPoolforWebSite -site $site.Name -apppool $site.AppPool.Name
 				
-			if( $site.SSL.enable -eq "true" )
-			{
+			if( $site.SSL.enable -eq "true" ) {
 				$working_directory = $PWD.Path
 				log -txt ( "Setting up SSL for " + $site.Name )
 				$secure_pass = ConvertTo-SecureString $site.SSL.pass -AsPlainText -Force 	
 				Import-PfxCertificate -certpath $site.SSL.pfx -pfxPass $secure_pass
 				Set-SSLforWebApplication -name $site.Name -common_name $site.SSL.subject
 				cd $working_directory
-					
-				log -txt "Auditing SSL Configuraiton"
-				&$audit_ssl_script -servers $servers -upload -parallel 
+				
+				if( $record ) { 
+					log -txt "Auditing SSL Configuraiton"
+					&$audit_ssl_script -servers $servers -upload -parallel 
+				}
 			}
 				
-			foreach( $command in $site.AdditionalCommands.command )
-			{
+			foreach( $command in $site.AdditionalCommands.command )	{
 				log -txt ( "Setting Additional Command " + $command.Name + " - " + $command.parameters )
 				Invoke-Expression ( $command.Name, $command.Parameters -join " ")
 			}
@@ -88,14 +88,19 @@ function main
 			Start-IISSite -computers "." -site $site.Name
 			
 			Get-Item ("IIS:\sites\" + $site.Name) | Select *
-				
-			log -txt ( "Syncing Farm " + $site.Name )
-			Sync-WebFarm $farm
-				
-			log -txt ( "Auditing IIS Site for " + $site.Name )
-			&$audit_iis_script -servers $servers -filter_type "url" -filter_value $site.Url -upload -farm $farm -env $env
+			
+			if( ! $nofarm ) {
+				log -txt ( "Syncing Farm " + $site.Name )
+				Sync-WebFarm $farm
+			}
+			
+			if( $record ) { 
+				log -txt ( "Auditing IIS Site for " + $site.Name )
+				&$audit_iis_script -servers $servers -filter_type "url" -filter_value $site.Url -upload -farm $farm -env $env
+			}
 		}
-	} catch [System.SystemException] {
+	} 
+	catch [System.SystemException] {
 		 Write-Host $_.Exception.ToString() -ForegroundColor Red
 	}
 }
