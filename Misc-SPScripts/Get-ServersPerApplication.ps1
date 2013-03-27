f774de9fedc785045dc7a40362954c154b6d7650
Brian Denicola <bjd145@yahoo.com>
2013-03-27 08:18:24 -0500

    Updated Get Servers Per Application Script.ps1

--- Misc-SPScripts/Get-ServersPerApplication.ps1
+++ Misc-SPScripts/Get-ServersPerApplication.ps1
@@ -19,50 +19,63 @@ function Split-DBServer([string] $server, [string] $port, [string] $instance)
     $ports = @( $port.Split(";#") | where { $_ -imatch "[0-9]+\..*" } )
     $instances = @( $instance.Split(";#") | Where { $_ -imatch "[A-Za-z]+" } )
 
-    $dbs = @()
-    for( $i=0; $i -lt $servers.Length; $i++ ) {
-        $dbs += "{0},{1}\{2}" -f ($servers[$i]).TrimEnd(), ([int]$ports[$i]).ToString().TrimEnd(), ($instances[$i]).TrimEnd()
+    $dbs = @()  
+    if( $servers -is [string] ) {
+        $dbs += "{0},{1}\{2}" -f $servers, ([int]$ports[0]).ToString(), $instances[0]
+    } else {
+        for( $i=0; $i -lt $servers.Length; $i++ ) {
+            $dbs += "{0},{1}\{2}" -f $servers[$i], ([int]$ports[$i]).ToString(), $instances[$i]
+        }
     }
-
     return $dbs
 }
     
 function main
 {
-    $app = Get-SPListViaWebService -url $url -list $list | Where Name -imatch $application 
+    $apps = Get-SPListViaWebService -url $url -list $list | Where Name -imatch $application 
 
     $systems = @()
-    foreach( $web_server in (Split-Server -server $app.WebServers) ) {
-        $systems += ( New-Object PSObject -Property @{
-            App = $app.Name
-            Type = "Web Server"
-            Server = $web_server
-        })
-    }
+    foreach( $app in $apps) {
+        foreach( $web_server in (Split-Servers -server $app.WebServers) ) {
+            $systems += ( New-Object PSObject -Property @{
+                App = $app.Name
+                Type = "Web Server"
+                Server = $web_server
+            })
+        }
 
-    foreach( $app_server in (Split-Server -server $app."Application Servers") ) {
-        $systems += ( New-Object PSObject -Property @{
-            App = $app.Name
-            Type = "Application Server"
-            Server = $app_server
-        })
-    }
+        if( ! [string]::IsNullorEmpty($app.IISFarm) -and $app.IISFarm -ne "StandAlone" ) {
+            $systems += ( New-Object PSObject -Property @{
+                App = $app.Name
+                Type = "Web Farm"
+                Server = $app.IISFarm
+            })
+        }
+
+        foreach( $app_server in (Split-Servers -server $app."Application Servers") ) {
+            $systems += ( New-Object PSObject -Property @{
+                App = $app.Name
+                Type = "Application Server"
+                Server = $app_server
+            })
+        }
     
-    foreach( $citrix_server in (Split-Server -server $app."Citrix Servers") ) {
-        $systems += ( New-Object PSObject -Property @{
-            App = $app.Name
-            Type = "Citrix Server"
-            Server = $citrix_server
-        })
-    }
+        foreach( $citrix_server in (Split-Servers -server $app."Citrix Servers") ) {
+            $systems += ( New-Object PSObject -Property @{
+                App = $app.Name
+                Type = "Citrix Server"
+                Server = $citrix_server
+            })
+        }
 
-    foreach( $db_server in (Split-DBServer -server $app."Database Server" -port $app."Database Server:Port" -instance $app."Database Server:Instance") ) {
-        $systems += ( New-Object PSObject -Property @{
-            App = $app.Name
-            Type = "Database Server"
-            Server = $db_server
-        })
+        foreach( $db_server in (Split-DBServer -server $app."Database Server" -port $app."Database Server:Port" -instance $app."Database Server:Instance") ) {
+            $systems += ( New-Object PSObject -Property @{
+                App = $app.Name
+                Type = "Database Server"
+                Server = $db_server
+            })
 
+        }
     }
     return $systems 
 }
