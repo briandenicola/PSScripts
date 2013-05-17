6f82a8d5691225ec764ccd3c0b49bf0faf499487
Brian Denicola <bjd145@yahoo.com>
2013-05-17 09:33:48 -0500

    Update User Profile Bug Fixes

--- Misc-SPScripts/Update-User-Profile-From-AD.ps1
+++ Misc-SPScripts/Update-User-Profile-From-AD.ps1
@@ -7,7 +7,7 @@
 . (Join-Path $ENV:SCRIPTS_HOME "\Libraries\SharePoint2010_functions.ps1")
 
 try {
-    $user = Get-SPuser -web $url -Limit ALL| where UserLogin -like $account 
+    $user = Get-SPuser -web $url -Limit ALL| where { $_.UserLogin -imatch $account }
     
     Write-Host "[ $(Get-Date) ] - $account setting before in $url site collection . . ."
     $user | Format-List
@@ -18,5 +18,5 @@ try {
     Get-SPUser -identity $user.ID -web $url | Format-List
 } 
 catch {
-    Write-Error "Failed with the following error - " + $_.Exception.ToString()
+    Write-Error ( "Failed with the following error - " + $_.Exception.ToString() )
 }
