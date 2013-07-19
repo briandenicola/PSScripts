658af0572c96d573c0d025467f37752a4cb3bc20
fmagoufis <fmagoufis@gmail.com>
2013-07-19 17:25:44 -0400

    Partition and Format Raw Disk
    This script will find newly added disks, assigned by HyperV.  Create a
    partition, assign next drive letter and format the disk using ntfs.

---
+++ DiskPartition/InitializePartitionFormatNewDisk.ps1
@@ -0,0 +1,5 @@
+Get-Disk |
+Where partitionstyle -eq 'raw' |
+Initialize-Disk -PartitionStyle MBR -PassThru |
+New-Partition -AssignDriveLetter -UseMaximumSize |
+Format-Volume -FileSystem NTFS -Confirm:$false
