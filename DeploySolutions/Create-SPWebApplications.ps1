8c2b72cf36091ad3c7bc22c93f942aeee4153259
Brian Denicola <bjd145@yahoo.com>
2014-02-03 20:23:34 -0600

    Updates to Create SP Application
    d

--- DeploySolutions/Create-SPWebApplications.ps1
+++ DeploySolutions/Create-SPWebApplications.ps1
@@ -1,8 +1,8 @@
 ﻿[CmdletBinding(SupportsShouldProcess=$true)]
 param (
 	[Parameter(Mandatory=$true)]
+    [ValidateScript({Test-Path $_})]
 	[string] $config,
-	
 	[switch] $reset
 )
 
@@ -10,7 +10,9 @@ param (
 . (Join-Path $ENV:SCRIPTS_HOME "Libraries\SharePoint_Functions.ps1")
 . (Join-Path $ENV:SCRIPTS_HOME "Libraries\SharePoint2010_Functions.ps1")
 
-$global:logFile = ".\web_application_creation-" +  $(Get-Date).ToString("yyyyMMdd") + ".log"
+Set-Variable -Name logFile -Value  (Join-Path $PWD.Path ("web_application_creation-" +  $(Get-Date).ToString("yyyyMMdd") + ".log"))
+Set-Variable -Name ssl -Value "443" -Option Constant
+Set-Variable -Name domain -Value "EXAMPLE"
 
 function HashTable_Output( [HashTable] $ht )
 {
@@ -21,88 +23,99 @@ function HashTable_Output( [HashTable] $ht )
 function log_provision( [string] $txt )
 {
 	Write-Verbose $txt
-	log -log $global:logFile -txt $txt
+	log -log $logFile -txt $txt
 }
 
-function main
+function Get-DatabaseServer
 {
-	Start-SPAssignment -Global
-	if ( -not ( Test-Path $config ) ) {
-		throw "Could not find $config"
-	}
-	$cfg = [xml] ( gc $config )
+    Set-Variable -Name index -Value 0
 	
-	$dbs_servers += @( Get-SPDatabase | where { $_.Type -eq "Content Database" } | Select -Expand Server -Unique )
-	if ( $dbs_servers.Count -eq 1 ) {
-		$index = 0
-	}
-	else {
-		$index = Get-Random -Min 0 -Max ($dbs_servers.Count-1)
-	}
-	$content_db_server = $dbs_servers[$index]
-
-	foreach( $webApp in $cfg.WebApplications.WebApp ) {
-		if( -not $webApp.ManagedAccount.Contains("USGTAD") ) {
-			$webApp.ManagedAccount = "USGTAD\" + $webApp.ManagedAccount
-		}
-
-		$acc = Get-SPManagedAccount $webApp.ManagedAccount -EA SilentlyContinue
-		if( $acc -eq $nul ) {
-			$cred = Get-Credential $webApp.ManagedAccount
-			New-SPManagedAccount $cred -verbose 
-			$acc = Get-SPManagedAccount $webApp.ManagedAccount
-		}
-
-		$options = @{
-			Name = $webApp.Name
-			Port = $webApp.Port
-			HostHeader = $webApp.HostHeader
-			URL = "http://" + $webApp.HostHeader
-			DatabaseName = $webApp.DatabaseName
-			DatabaseServer = $content_db_server
-			ApplicationPool = "AppPool - " + $webApp.HostHeader
-			ApplicationPoolAccount = $acc
-		}
-	
-		$aps = @()
-		foreach( $auth in $webApp.Auth.type ) {
-			if($auth.Name -eq "NTLM" ) {
-				$aps += New-SPAuthenticationProvider
-			}
-			elseif($auth.Name -eq "FBA" ) {
-				$aps += New-SPAuthenticationProvider -ASPNETMembershipProvider $auth.provider -ASPNETRoleProviderName $auth.role
-			}  
-		}
-	
-		if( $aps.count -gt 0 ) {
-			$options.Add( 'AuthenticationProvider', $aps )
-		}
-
-		log_provision -txt ("Going to create a new web application with the following options - " + (HashTable_Output $options)) 
-		New-SPWebApplication @options -verbose 
-	
-		if( $webApp.ExtendHostHeader -ne $nul) {
-			$extendoptions = @{
-				Name = $webApp.ExtendName
-				Port = $webApp.ExtendPort
-				HostHeader = $webApp.ExtendHostHeader
-				URL = "http://" + $webApp.ExtendHostHeader
-				zone = $webApp.ExtendZone
-			}
-			$AppURL = "http://" + $webApp.HostHeader
-			log_provision -txt ("Going to extend web application with the following options - " + (HashTable_Output $extendoptions))
-			Get-SPWebApplication -Identity $AppURL | New-SPWebApplicationExtension @ExtendOptions -verbose
-		}
+    $dbs_servers = @( Get-SPDatabase | where { $_.Type -eq "Content Database" } | Select -Expand Server -Unique )
+	if ( $dbs_servers.Count -ne 1 ) {
+		$index = Get-Random -Min 0 -Max ($dbs_servers.Count-1) 
 	}
 
-	if( $reset ) {
-		$web_servers = @( Get-SPServiceInstance | where { $_.TypeName -eq "Microsoft SharePoint Foundation Web Application" -and $_.Status -eq "Online" }  | Select Server )
-		foreach( $server in $web_servers ) {
-			log_provision -txt ("Resetting IIS on - " + $_.Server.Address ) 
-			iisreset $_.Server.Address 
-		}
-	} 
-	Stop-SPAssignment -Global 
+	return $dbs_servers[$index]
 }
-main
 
+$cfg = [xml] ( Get-Content $config )
+$content_db_server = Get-DatabaseServer
+
+foreach( $webApp in $cfg.WebApplications.WebApp ) {
+	if( -not $webApp.ManagedAccount.Contains($domain) ) {
+		$webApp.ManagedAccount = "{0}\{1}" -f $domain,$webApp.ManagedAccount
+	}
+
+	$managed_account = Get-SPManagedAccount $webApp.ManagedAccount -EA SilentlyContinue
+	if( $managed_account -eq $nul ) {
+		$cred = Get-Credential $webApp.ManagedAccount
+		New-SPManagedAccount $cred -verbose 
+		$managed_account = Get-SPManagedAccount $webApp.ManagedAccount
+	}
+
+	$options = @{
+		Name = $webApp.Name
+		Port = $webApp.Port
+		HostHeader = $webApp.HostHeader
+		DatabaseName = $webApp.DatabaseName
+		DatabaseServer = $content_db_server
+		ApplicationPool = "AppPool - " + $webApp.HostHeader
+		ApplicationPoolAccount = $managed_account
+	}
+	
+    if( $webApp.Port -eq $ssl ) {
+        $app_url = "https://{0}" -f $webApp.HostHeader
+        $options.Add('URL', $app_url)
+        $options.Add('SecureSocketsLayer', $true)
+    }
+    else {
+        $app_url = "http://{0}" -f $webApp.HostHeader
+        $options.Add('URL', $app_url)
+    }
+
+	$auth_provider = @()
+	foreach( $auth in $webApp.Auth.type ) {
+		if($auth.Name -eq "NTLM") {
+			$auth_provider += New-SPAuthenticationProvider
+		}
+		elseif($auth.Name -eq "FBA") {
+			$auth_provider += New-SPAuthenticationProvider -ASPNETMembershipProvider $auth.provider -ASPNETRoleProviderName $auth.role
+		}  
+	}
+	
+	if( $auth_provider.count -gt 0 ) {
+		$options.Add( 'AuthenticationProvider', $auth_provider )
+	}
+
+	log_provision -txt ("Going to create a new web application with the following options - " + (HashTable_Output $options)) 
+	New-SPWebApplication @options -verbose 
+	
+	if( $webApp.ExtendHostHeader -ne $nul ) {
+		$extendoptions = @{
+			Name = $webApp.ExtendName
+			Port = $webApp.ExtendPort
+			HostHeader = $webApp.ExtendHostHeader
+			Zone = $webApp.ExtendZone
+		}
+
+        if( $webApp.ExtendPort -eq $ssl ) {
+            $extendoptions.Add('URL', ("https://{0}" -f $webApp.ExtendHostHeader))
+            $extendoptions.Add('SecureSocketsLayer', $true)
+        }
+        else {
+            $app_url = 
+            $extendoptions.Add('URL', ("http://{0}" -f $webApp.ExtendHostHeader))
+        }
+
+		log_provision -txt ("Going to extend web application with the following options - " + (HashTable_Output $extendoptions))
+		Get-SPWebApplication -Identity $app_url | New-SPWebApplicationExtension @ExtendOptions -verbose
+	}
+}
+
+if( $reset ) {
+	$web_servers = @( Get-SPServiceInstance | where { $_.TypeName -eq "Microsoft SharePoint Foundation Web Application" -and $_.Status -eq "Online" }  | Select Server )
+	foreach( $server in $web_servers ) {
+		log_provision -txt ( "Resetting IIS on - " + $_.Server.Address ) 
+		iisreset $_.Server.Address 
+	}
+} 
--- Libraries/Azure_Functions.ps1
+++ Libraries/Azure_Functions.ps1
@@ -2,4 +2,29 @@
 Get-ChildItem 'C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\Azure\*.psd1' | ForEach-Object {Import-Module $_}
 Import-Module MSOnline -DisableNameChecking
 Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
-Pop-Location 
+Pop-Locaton
+
+function Install-WinRmCertificate
+{
+    param{
+        [string] $service, 
+        [string] $vm_name
+    }
+    
+    Set-Variable -Name cert_store -Value (New-Object System.Security.Cryptography.X509Certificates.X509Store "Root", "LocalMachine")
+        
+    $vm = Get-AzureVM -ServiceName $service -Name $vm_name 
+    $winrm_cert = Get-AzureCertificate -ServiceName $service -Thumbprint ($vm.VM.DefaultWinRMCertificateThumbprint) -ThumbprintAlgorithm sha1
+    
+    $cert = Get-Item (Join-Path "cert:\CurrentUser\My\" $vm.VM.DefaultWinRMCertificateThumbprint) -ErrorAction SilentlyContinue
+    
+    if(!$cert) {
+        $cert_base64 = [System.Convert]::FromBase64String($winrm_cert.Data)
+        $x509_cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate
+        $x509_cert.Import($cert_base64)
+        
+        $cert_store.Open("ReadWrite")
+        $cert_store.Add($x509_cert)
+        $cert_store.Close()
+    }
+} 
