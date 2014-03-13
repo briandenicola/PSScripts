a9382976340c531322579abad4d1fb2d1ec55741
Brian Denicola <bjd145@yahoo.com>
2014-03-12 21:13:40 -0500

    Added another module to Azure_Functions

--- Libraries/Azure_Functions.ps1
+++ Libraries/Azure_Functions.ps1
@@ -1,6 +1,7 @@
 ﻿Push-Location $PWD.Path
 Get-ChildItem 'C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\Azure\*.psd1' | ForEach-Object {Import-Module $_}
 Import-Module MSOnline -DisableNameChecking
+Import-Module MSOnlineExtended -DisableNameChecking
 Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
 Pop-Location
 
