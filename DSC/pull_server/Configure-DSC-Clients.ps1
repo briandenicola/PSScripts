61990c30f59b6bd796b73ea408ed49e739ab0647
Brian Denicola <bjd145@yahoo.com>
2014-03-24 10:38:50 -0500

    bug fixes to dsc automation scripts.

--- DSC/pull_server/Configure-DSC-Clients.ps1
+++ DSC/pull_server/Configure-DSC-Clients.ps1
@@ -1,5 +1,5 @@
 ﻿param(    [string] $pull_server)configuration LetsGetConfiguring{    param ($NodeId, $PullServer)    
-    LocalConfigurationManager    {        AllowModuleOverwrite = 'True'        ConfigurationID = $NodeId        ConfigurationModeFrequencyMins = 60         ConfigurationMode = 'ApplyAndAutoCorrect'        RebootNodeIfNeeded = 'True'        RefreshMode = 'PULL'         DownloadManagerName = 'WebDownloadManager'        DownloadManagerCustomData = (@{ServerUrl = "http://$PullServer/psdscpullserver.svc"})    }}
+    LocalConfigurationManager    {        AllowModuleOverwrite = 'True'        ConfigurationID = $NodeId        ConfigurationModeFrequencyMins = 30        ConfigurationMode = 'ApplyAndAutoCorrect'        RebootNodeIfNeeded = 'True'        RefreshMode = 'PULL'         DownloadManagerName = 'WebDownloadManager'        DownloadManagerCustomData = (@{ServerUrl = "https://$PullServer/psdscpullserver.svc"})    }}
 
 $node = [guid]::NewGuid().Guid
 LetsGetConfiguring -NodeId $node.ToString() -PullServer $pull_serverSet-DscLocalConfigurationManager -path LetsGetConfiguring
--- System-Setup-Workflow-With-DSC/Setup-NewComputer-Workflow.ps1
+++ System-Setup-Workflow-With-DSC/Setup-NewComputer-Workflow.ps1
@@ -11,7 +11,8 @@ function Set-MOFHash {
         [string] $module
     )
 
-    Set-Variable -Name mof -Value ( Join-Path $PWD.Path ("{0}\{1}.mof" -f $module, $guid) )
+    Set-Variable -Name dsc_path -Value "C:\Program Files\WindowsPowerShell\DscService\Configuration\" -Option Constant
+    Set-Variable -Name mof -Value ( Join-Path $PWD.Path ("{0}\{1}.mof" -f $module,$guid) )
     Set-Variable -Name checksum -Value ( Join-Path $PWD.Path ("{0}\{1}.mof.checksum" -f $module, $guid) )
     
     if( !(Test-path $mof) ) {
@@ -20,6 +21,9 @@ function Set-MOFHash {
        
     $hash = Get-FileHash $mof
     [System.IO.File]::AppendAllText( $checksum, $hash.Hash )
+
+    Copy-Item $mof $dsc_path -Verbose -Force
+    Copy-Item $checksum $dsc_path -Verbose -Force
 }
 
 Set-Variable -Name password     -Value (ConvertTo-SecureString "01000000d08c9ddf0115d1118c7a00c04fc297eb01000000c4a0897a6f52d14a992239db193c3b5800000000020000000000106600000001000020000000f898c9f7e2ebeb05b2ede10d45f236451b25361ef92b68b9281f48349d10ea8e000000000e8000000002000020000000aa728a19476afc227e471924f69e057bf8c29e27c65d13de39146cdda658098f2000000071e06b802e24b41dbd4cdb53372b7602da678571ed1fba8a079854a14fb8320940000000cc64479cfe2a4549382212f10c21ccc01dfa9de3b91b861fd0d40038f3d08b403d3b9d95cde94976beb9718778787f8af514a5d9d9373af1be46e1436505d014") -Option Constant
@@ -29,9 +33,11 @@ Set-Variable -Name local_creds  -value (New-Object System.Management.Automation.
 Set-Variable -Name node         -Value ([guid]::NewGuid() | Select -Expand Guid)
 
 . (Join-Path $PWD.Path "Modules\Setup-Workflow.ps1")
-. (Join-Path $PWD.Path "Modules\IIS-Server-DSC-Template.ps1") -Nodeid $node 
+. (Join-Path $PWD.Path "Modules\Server-DSC-Template.ps1") -Nodeid $node 
 
 Write-Host ("Using Guid - {0} for {1}. Please save. Value will also be saved on the root of the C: parition on {1}." -f $node, $computer_name)
+
+winrm s winrm/config/client ('@{TrustedHosts="' + $computer_name + '"}')
 
 #Workflow to Setup Machine
 $options = @{ 
@@ -40,12 +46,9 @@ $options = @{
     pull_server = $pull_server
     guid = $node
     cred = $domain_creds
-    PSPersist = $true 
-    PSComputerName = $computer_name
-    PSCredential = $local_creds
 }
-Setup-NewComputer @options -Verbose
+Setup-NewComputer @options -PSPersist $true -PSComputerName $computer_name -PSCredential $local_creds -Verbose
 
 #DSC To Configure
-IISServerSetup
-Set-MOFHash -guid $node -Module "IISServerSetup"
+ServerSetup
+Set-MOFHash -guid $node -Module "ServerSetup"
