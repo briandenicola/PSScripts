6d13f9b3774d08e3053242ad3939df70375532a6
Brian Denicola <bjd145@yahoo.com>
2014-05-21 23:56:26 -0500

    Updated DSC Script based on new info from TechED

--- System-Setup-Workflow-With-DSC/Setup-NewComputer-Workflow.ps1
+++ System-Setup-Workflow-With-DSC/Setup-NewComputer-Workflow.ps1
@@ -1,8 +1,30 @@
 ﻿param (
  [Parameter(Mandatory=$true)]
  [string] $computer_name,
+
+ [Parameter(Mandatory=$true)]
  [string] $new_name,
- [string] $domain = "sharepoint.test"
+
+ [Parameter(Mandatory=$true)]
+ [string] $domain = "sharepoint.test",
+
+ [Parameter(Mandatory=$true)]
+ [string] $node,
+
+ [Parameter(Mandatory=$true)]
+ [string] $pull_server = "dsc.sharepoint.test",
+
+ [Parameter(Mandatory=$true)]
+ [string] $local_user,
+
+ [Parameter(Mandatory=$true)]
+ [string] $local_password,
+
+ [Parameter(Mandatory=$true)]
+ [string] $domain_user,
+
+ [Parameter(Mandatory=$true)]
+ [string] $domain_password
 )
 
 function Set-MOFHash {
@@ -16,7 +38,7 @@ function Set-MOFHash {
     Set-Variable -Name checksum -Value ( Join-Path $PWD.Path ("{0}\{1}.mof.checksum" -f $module, $guid) )
     
     if( !(Test-path $mof) ) {
-        throw "Coudl not find $mof . . "
+        throw "Could not find $mof . . "
     }
        
     $hash = Get-FileHash $mof
@@ -26,11 +48,12 @@ function Set-MOFHash {
     Copy-Item $checksum $dsc_path -Verbose -Force
 }
 
-Set-Variable -Name password     -Value (ConvertTo-SecureString "01000000d08c9ddf0115d1118c7a00c04fc297eb01000000c4a0897a6f52d14a992239db193c3b5800000000020000000000106600000001000020000000f898c9f7e2ebeb05b2ede10d45f236451b25361ef92b68b9281f48349d10ea8e000000000e8000000002000020000000aa728a19476afc227e471924f69e057bf8c29e27c65d13de39146cdda658098f2000000071e06b802e24b41dbd4cdb53372b7602da678571ed1fba8a079854a14fb8320940000000cc64479cfe2a4549382212f10c21ccc01dfa9de3b91b861fd0d40038f3d08b403d3b9d95cde94976beb9718778787f8af514a5d9d9373af1be46e1436505d014") -Option Constant
-Set-Variable -Name pull_server  -Value ("dsc." + $domain) -Option Constant
-Set-Variable -Name domain_creds -value (New-Object System.Management.Automation.PSCredential (($domain + "\brian-a"), $password))
-Set-Variable -Name local_creds  -value (New-Object System.Management.Automation.PSCredential ("administrator", $password))
-Set-Variable -Name node         -Value ([guid]::NewGuid() | Select -Expand Guid)
+Set-Variable -Name domain_creds -value (New-Object System.Management.Automation.PSCredential ($domain_user, (ConvertTo-SecureString $domain_password -AsPlainText -Force)))
+Set-Variable -Name local_creds  -value (New-Object System.Management.Automation.PSCredential ($local_user, (ConvertTo-SecureString $local_password -AsPlainText -Force)))
+
+if( [string]::IsNullOrEmpty($node) ) { 
+    Set-Variable -Name node -Value ([guid]::NewGuid() | Select -Expand Guid)
+}
 
 . (Join-Path $PWD.Path "Modules\Setup-Workflow.ps1")
 . (Join-Path $PWD.Path "Modules\Server-DSC-Template.ps1") -Nodeid $node 
