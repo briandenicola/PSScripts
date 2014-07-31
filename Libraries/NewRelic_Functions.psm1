a776c450de5995bc505b3f179f2b192e98072f5b
Brian Denicola <bjd145@yahoo.com>
2014-07-30 20:46:07 -0500

    New Relic API 0.0.2
    Some code refactoring
    Preping to move to V2 API instead of V1 API

--- Libraries/NewRelic_Functions.psm1
+++ Libraries/NewRelic_Functions.psm1
@@ -1,16 +1,32 @@
 Set-Variable -Name nr_api_key -Value "" -Option Constant
 Set-Variable -Name nr_account_id -Value "" -Option Constant
-
-Set-Variable -Name nr_url_apps -Value "https://api.newrelic.com/api/v1/accounts/{0}/applications.xml" -Option Constant
-Set-Variable -Name nr_url_deploy -Value "https://api.newrelic.com/deployments.xml" -Option Constant
 Set-Variable -Name nr_web_config_key -Value "NewRelic.AppName" -Option Constant
-Set-Variable -Name nr_deploy_description  -Value "[{0}] - Deployment for {1} ({2}) by {3}"
+
+$nr_urls = New-Object PSObject -Property @{
+    Applications ="https://api.newrelic.com/api/v1/accounts/{0}/applications.xml"
+    Deployment="https://api.newrelic.com/deployments.xml"
+}
+
+$nr_output = ConvertFrom-StringData @'
+    Deployment="[{0}] - Deployment for {1} ({2}) by {3}"
+'@
+
+$nr_error_data = ConvertFrom-StringData @'    
+    NoAppName=No New Relic Application was found of Name : {0}.
+    NoAppSetting=Could not find {0} in {1} web.config's AppSettings.
+'@
+
+function Get-NewRelicWebClient
+{
+    $web_client = New-Object System.Net.WebClient
+    $web_client.Headers.Add("x-api-key", $nr_api_key)
+    return $web_client
+}
 
 function Get-NewRelicApplications
 {
-    $wc = New-Object System.Net.WebClient
-    $wc.Headers.Add("x-api-key", $nr_api_key)
-    $apps = [xml] $wc.DownloadString($nr_url_apps -f $nr_account_id)
+    $wc = Get-NewRelicWebClient
+    $apps = [xml] $wc.DownloadString($nr_url.Applications -f $nr_account_id)
     
     $nr_apps = @()
     foreach ($application in $apps.applications.application)
@@ -18,7 +34,6 @@ function Get-NewRelicApplications
 	    $nr_apps += (New-Object PSObject -Property @{
             Name = $application.Name
             ID = $application.Id.'#text'
-            Url = $application.'OverView-Url'
         })
     }
     
@@ -41,6 +56,10 @@ function Get-NewRelicAppName
                 Where { $_.Key -eq $nr_web_config_key } | 
                 Select -ExpandProperty Value
 
+    if( [String]::IsNullOrEmpty($nr_app_name) ) {
+        throw ( $nr_error_data.NoAppSetting -f $nr_web_config_key, $iis_site_name )
+    }
+
     return $nr_app_name
 }
 
@@ -55,14 +74,16 @@ function Set-NewRelicDeploymentMarker
 		Where { $_.Name -eq (Get-NewRelicAppName -iis_site_name $iis_site_name) } |
 		Select -Expand ID
 	
-   	$wc = new-object system.net.WebClient 
-    $wc.Headers.Add("x-api-key", $nr_api_key)
+    if( $app_id -eq $null ) {
+        throw ( $nr_error_data.NoAppName -f $iis_site_name )
+    }
 
-   	$deployment_info = new-object System.Collections.Specialized.NameValueCollection 
+   	$wc = Get-NewRelicWebClient
+   	$deployment_info = New-Object System.Collections.Specialized.NameValueCollection 
 	$deployment_info.Add("application_id", $app_id ) 
-	$deployment_info.Add("description", ($nr_deploy_description -f $(Get-Date), $iis_site_name, $app_id, $ENV:USERNAME) )
+	$deployment_info.Add("description", ($nr_output.Deployment -f $(Get-Date), $iis_site_name, $app_id, $ENV:USERNAME) )
 	$deployment_info.Add("user", ("{0}\{1}" -f $env:USERDOMAIN, $ENV:USERNAME ) ) 
-	$result = $wc.UploadValues($nr_url_deploy, "POST", $deployment_info) 
+	$result = $wc.UploadValues($nr_urls.Deployment, "POST", $deployment_info) 
 
     return ( [System.Text.Encoding]::ASCII.GetString($result) )
 
