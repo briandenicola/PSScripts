9f6092144cb79a96ac42909cd7d5fcb2e0d22b3c
bjd145 <bjd145@yahoo.com>
2014-08-12 08:03:35 -0500

    New Relic API 0.0.3
    Functions for Servers Info and Application Details

--- Libraries/NewRelic_Functions.psm1
+++ Libraries/NewRelic_Functions.psm1
@@ -3,18 +3,22 @@ Set-Variable -Name nr_account_id -Value "" -Option Constant
 Set-Variable -Name nr_web_config_key -Value "NewRelic.AppName" -Option Constant
 
 $nr_urls = New-Object PSObject -Property @{
-    Applications ="https://api.newrelic.com/api/v1/accounts/{0}/applications.xml"
+    Applications="https://api.newrelic.com/v2/applications.xml"
+    Application="https://api.newrelic.com/v2/applications/{0}.xml"
     Deployment="https://api.newrelic.com/deployments.xml"
+    Servers="https://api.newrelic.com/v2/servers.xml"
+    Server="https://api.newrelic.com/v2/servers/{0}.xml"
 }
 
-$nr_output = ConvertFrom-StringData @'
+$nr_output = New-Object PSObject -Property @{
     Deployment="[{0}] - Deployment for {1} ({2}) by {3}"
-'@
+}
 
-$nr_error_data = ConvertFrom-StringData @'    
-    NoAppName=No New Relic Application was found of Name : {0}.
-    NoAppSetting=Could not find {0} in {1} web.config's AppSettings.
-'@
+$nr_error_data = New-Object PSObject -Property @{   
+    NoAppName="No New Relic Application was found of Name : {0}."
+    NoAppId="No New Relic Application was found of ID : {0}."
+    NoAppSetting="Could not find {0} in {1} web.config's AppSettings."
+}
 
 function Get-NewRelicWebClient
 {
@@ -26,19 +30,87 @@ function Get-NewRelicWebClient
 function Get-NewRelicApplications
 {
     $wc = Get-NewRelicWebClient
-    $apps = [xml] $wc.DownloadString($nr_url.Applications -f $nr_account_id)
+    $apps = [xml] $wc.DownloadString($nr_urls.Applications)
     
     $nr_apps = @()
-    foreach ($application in $apps.applications.application)
-    {
+    foreach ($application in $apps.applications_response.applications.application) {
 	    $nr_apps += (New-Object PSObject -Property @{
             Name = $application.Name
-            ID = $application.Id.'#text'
+            ID = $application.id
+            Status = $application.Health_Status
+            Reporting = $application.Reporting
+            LastReported = $application.last_reported_at
         })
     }
     
     return $nr_apps
 }
+
+function Get-NewRelicApplicationDetails
+{
+    param(
+        [Parameter(ParameterSetName="Name",Mandatory=$true)]
+        [string] $name,
+
+        [Parameter(ParameterSetName="ID",Mandatory=$true)]
+        [int] $id
+    )
+
+
+    if($PsCmdlet.ParameterSetName -eq "Name") {
+        $id = Get-NewRelicApplications | Where { $_.Name -eq $Name } | Select -ExpandProperty Id
+
+        if(!$id) {
+            throw ($nr_error_data.NoAppName -f $name)
+        }
+    }
+
+    $wc = Get-NewRelicWebClient
+    $response = [xml] $wc.DownloadString($nr_urls.Application -f $id) | Out-Null
+
+    if(!$response) {
+        throw ($nr_error_data.NoAppId -f $id)
+    }
+
+    $nr_app = New-Object PSObject -Property @{
+            Name = $response.application_response.application.Name
+            ID = $response.application_response.application.id
+            Status = $response.application_response.application.Health_Status
+            Reporting = $response.application_response.application.Reporting
+            LastReported = $response.application_response.application.last_reported_at
+            ResponseTime = $response.application_response.application.application_summary.response_time
+            ApdexScore = $response.application_response.application.application_summary.apdex_score
+            ApdexTarget = $response.application_response.application.application_summary.apdex_target
+            ErrorRate = $response.application_response.application.application_summary.error_rate
+            ServerIds = @($response.application_response.application.links.servers.server)
+    }
+
+    return $nr_app
+}
+
+function Get-NewRelicServers 
+{
+    $wc = Get-NewRelicWebClient
+    $servers = [xml] $wc.DownloadString($nr_urls.Servers)
+    
+    $nr_servers = @()
+    foreach ($server in $servers.servers_response.servers.server) {
+	    $nr_servers += (New-Object PSObject -Property @{
+            Name = $server.Name
+            ID = $server.id
+            Reporting = $server.Reporting
+            LastReported = $server.last_reported_at
+            CPU = $server.summary.CPU
+            IO = $server.summary.disk_io
+            MemoryUsedByPercent = $server.summary.memory
+        })
+    }
+    
+    return $nr_servers
+
+}
+
+function Get-NewRelicServerDetails {}
 
 function Get-NewRelicAppName
 {
@@ -90,4 +162,4 @@ function Set-NewRelicDeploymentMarker
 }
 
 
-Export-ModuleMember -Function Set-NewRelicDeploymentMarker, Get-NewRelicApplications, Get-NewRelicAppName
+Export-ModuleMember -Function Set-NewRelicDeploymentMarker, Get-NewRelicApplications, Get-NewRelicApplicationDetails, Get-NewRelicAppName, Get-NewRelicServers, Get-NewRelicServerDetails
