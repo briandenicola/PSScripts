8ba313ee609afae803b9bc224ef9bdc3a38314bb
Brian Denicola <bjd145@yahoo.com>
2015-01-28 12:56:45 -0600

    Create Environment - Coding 0.0.3

--- Azure/Create-Environment/Modules/Azure-VirtualMachines-Functions.psm1
+++ Azure/Create-Environment/Modules/Azure-VirtualMachines-Functions.psm1
@@ -13,26 +13,42 @@ function New-AzureVirtualMachine
         [Parameter(Mandatory=$true)][string] $AdminUser = "Administrator",
         [Parameter(Mandatory=$true)][string] $AdminPassword,
         [Parameter(Mandatory=$true)][string] $SubnetName,
+        [Parameter(Mandatory=$true)][string] $AffinityGroup,
         [Parameter(Mandatory=$false)][string] $ScriptExtensionUri,
         [Parameter(Mandatory=$false)][System.Collections.Hashtable[]] $EndPoints,
         [Parameter(Mandatory=$false)][System.Collections.Hashtable[]] $DataDrives,
         [Parameter(ParameterSetName="JoinDomain",Mandatory=$true)][string] $DomainUser,
-        [Parameter(ParameterSetName="JoinDomain",Mandatory=$true)][string] $DomainPassword
+        [Parameter(ParameterSetName="JoinDomain",Mandatory=$true)][string] $DomainPassword,
+        [Parameter(ParameterSetName="JoinDomain",Mandatory=$true)][string] $Domain
     )
 
-    $image = Get-LatestAzureVMImageName -image_family_name $OperatingSystem
+    Set-Variable -Name lun -Value 0
+    Set-Variable -Name script_to_run -Value ($ScriptExtensionUri.Split("/") | Select -Last 1)
+    Set-Variable -Name image -Value (Get-LatestAzureVMImageName -image_family_name $OperatingSystem)
 
-    $vm = New-AzureVMConfig -Name $Name -InstanceSize $Size -ImageName $image |
-        Add-AzureProvisioningConfig -Windows -Password $AdminPassword -AdminUsername $AdminUser |
-        Set-AzureSubnet -SubnetNames $SubnetName  
-    
+    $vm = New-AzureVMConfig -Name $Name -InstanceSize $Size -ImageName $image | Set-AzureSubnet -SubnetNames $SubnetName  
+        
+    switch ($PsCmdlet.ParameterSetName)
+    { 
+        "JoinDomain" {
+            $vm | Add-AzureProvisioningConfig -Windows -Password $AdminPassword -AdminUsername $AdminUser -DomainPassword $DomainPassword -DomainUserName $DomainUser -JoinDomain $Domain
+        }
+        default {
+            $vm | Add-AzureProvisioningConfig -Windows -Password $AdminPassword -AdminUsername $AdminUser 
+            
+        }
+    }
+   
     foreach( $drive in $DataDrives ) {
-        $vm | Add-AzureDataDisk -ImportFrom -MediaLocation $storage_url -LUN 1 -DiskLabel "DATA"
+        $vm | Add-AzureDataDisk -CreateNew -DiskSizeInGB $drive.DriveSize -DiskLabel $drive.DriveLabel -LUN $lun++
     }
 
-    foreach( $drive in $DataDrives ) {
-        $vm | Add-AzureDataDisk -ImportFrom -MediaLocation $storage_url -LUN 1 -DiskLabel "DATA"
+    foreach( $endpoint in $EntPoints ) {
+        $vm | Add-AzureEndpoint -Name $endpoint.Name -LocalPort $endpoint.LocalPort -PublicPort $endpoint.RemotePort -Protocol TCP
     }
+
+    $vm | New-AzureVM -ServiceName $CloudService -AffinityGroup $AffinityGroup -WaitForBoot
+    $vm | Set-AzureVMCustomScriptExtension -FileUri $ScriptExtensionUri -Run $script_to_run | Update-AzureVM
 }
 
 Export-ModuleMember -Function New-AzureVirtualMachine
