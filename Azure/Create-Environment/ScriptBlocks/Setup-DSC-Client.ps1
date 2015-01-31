27f3db1d9b94ea61ca25dcee7b613f982b189423
Brian Denicola <bjd145@yahoo.com>
2015-01-30 22:18:38 -0600

    Create Environment - 0.2.1

--- Azure/Create-Environment/ScriptBlocks/Setup-DSC-Client.ps1
+++ Azure/Create-Environment/ScriptBlocks/Setup-DSC-Client.ps1
@@ -7,13 +7,19 @@ configuration ConfigureDSCPullServer {    param (
         [string] $NodeId, 
         [string] $PullServer
     )  
-      
-    LocalConfigurationManager    {        AllowModuleOverwrite = 'True'        ConfigurationID = $NodeId        ConfigurationModeFrequencyMins = 30         ConfigurationMode = 'ApplyAndAutoCorrect'        RebootNodeIfNeeded = 'True'        RefreshMode = 'PULL'         DownloadManagerName = 'WebDownloadManager'        DownloadManagerCustomData = @{            ServerUrl = "http://$PullServer/psdscpullserver.svc"            AllowUnsecureConnection = ‘true’        }               }}
+    
+    Node "localhost"
+    {
+        LocalConfigurationManager        {            AllowModuleOverwrite = 'True'            ConfigurationID = $NodeId            ConfigurationModeFrequencyMins = 30             ConfigurationMode = 'ApplyAndAutoCorrect'            RebootNodeIfNeeded = 'True'            RefreshMode = 'PULL'             DownloadManagerName = 'WebDownloadManager'            DownloadManagerCustomData = @{                ServerUrl = "http://$PullServer/psdscpullserver.svc"                AllowUnsecureConnection = ‘true’            }          }    }}
 
 Get-Disk | Where { $_.PartitionStyle -eq "RAW" } | Initialize-Disk -PartitionStyle MBR 
 Get-Disk | Where { $_.NumberOfPartitions -eq 0 } |   
     New-Partition -AssignDriveLetter -UseMaximumSize |
     Format-Volume -FileSystem NTFS -Force -Confirm:$false
 
-ConfigureDSCPullServer -NodeId $guid -PullServer $pull_serverSet-DscLocalConfigurationManager -path ConfigureDSCPullServer
-$guid | Add-Content -Encoding Ascii -Path ( Join-Path -Path "C:" -ChildPath $guid ) 
+$dsc_path = Join-Path -Path $ENV:SystemDrive -ChildPath "DSCResources"
+New-Item -Path $dsc_path -ItemType Directory 
+Set-Location -Path $dsc_path
+
+ConfigureDSCPullServer -NodeId $guid -PullServer $pull_serverSet-DscLocalConfigurationManager -Path ConfigureDSCPullServer -ComputerName localhost 
+Add-Content -Encoding Ascii -Path ( Join-Path -Path $ENV:SystemDrive  -ChildPath $guid ) -Value $guid
