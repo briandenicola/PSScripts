ace8b3afe84c29fcb9ba97d10b7721c88ae0a604
Brian Denicola <bjd145@yahoo.com>
2015-03-11 22:16:10 -0500

    Refactor SP2010 Deploy Thesarus Script

--- SharePoint/Search/Deploy-Dictionary-Thesaurus.ps1
+++ SharePoint/Search/Deploy-Dictionary-Thesaurus.ps1
@@ -1,102 +1,88 @@
 ﻿[CmdletBinding(SupportsShouldProcess=$true)]
 param (
-	[Parameter(Mandatory=$true)]
-	[string] $file,
+    [ValidateScript({Test-Path $_})] 
+	[Parameter(ParameterSetName="Dictionary",Mandatory=$true)][string] $DictionaryFile,	
 	
-	[Parameter(Mandatory=$true)]
-	[ValidateSet("dictionary", "thesaurus")]
-	[string] $deploy 
+    [ValidateScript({Test-Path $_})] 
+    [Parameter(ParameterSetName="Thesaurus", Mandatory=$true)][string] $ThesaurusFile
 )
 
-Add-PSSnapin Microsoft.SharePoint.PowerShell –erroraction SilentlyContinue
+. (Join-Path $ENV:SCRIPTS_HOME "Libraries\SharePoint2010_Functions.ps1")
 
-$now = $(Get-Date).ToString("yyyyMMddhhmmss")
+Set-Variable -Name now             -Value $(Get-Date).ToString("yyyyMMddhhmmss") -Option Constant
+Set-Variable -Name dictionary_file -Value "custom0009.lex"                       -Option Constant
+Set-Variable -Name thesaurus_file  -Value "tsenu.xml"                            -Option Constant
 
-function Get-QueryComponents()
+Set-Variable -Name dictionary_path        -Value "d$\SharePoint\14\14.0\Bin\"           -Option Constant 
+Set-Variable -Name default_thesaurus_path -Value @("c$\Program Files\Microsoft Office Servers\14.0\Data\Office Server\Config",
+		                                           "c$\Program Files\Microsoft Office Servers\14.0\Data\Config") -Option Constant
+function Get-QueryComponents
 {
-	$query_topology = Get-SPServiceApplication | where { $_.TypeName -eq "Search Service Application" } | Get-SPEnterpriseSearchQueryTopology 
-	return ( $query_topology | where { $_.State -eq "Active" } | Select QueryComponents )	
+	return( Get-SPServiceApplication | 
+        Where { $_.TypeName -eq "Search Service Application" } | 
+        Get-SPEnterpriseSearchQueryTopology  | 
+        Where { $_.State -eq "Active" } |
+        Select QueryComponents )
 }
 
-function DeployTo-SharePointALL ( [string] $new_file, [string[]] $path, [string] $default_file )
+function DeployTo-SharePointALL 
 {
-		Get-SPServer | where { $_.Role -ne "Invalid" } | % { 
-			$system = $_.Address 
-			$path | % {
-				$org_file =  "\\" + $system  + "\" + $_ + "\" + $default_file
-				$backup_file = $org_file + "." + $now
+    param(
+        [string]   $new_file, 
+        [string[]] $paths,
+        [string]   $default_file 
+    )
 
-				if( Test-Path $org_file )
-				{
-					copy $org_file $backup_file -verbose
-				}
-				copy $new_file $org_file -Force -verbose
+	foreach( $system in (Get-SPServer | Where { $_.Role -ne "Invalid" } | Select -ExpandProperty Address) ) {
+		foreach( $path in $paths ) {
+			$org_file =  "\\{0}\{1}\{2}" -f $system,$path, $default_file
+			$backup_file = $org_file + "." + $now
+
+			if( Test-Path $org_file ) {
+				Copy -Path $org_file -Destination $backup_file -verbose
 			}
+			Copy Path $new_file -Destination $org_file -Force -verbose
 		}
+	}
 }
 
-function Restart-Search ( [String[]] $systems )
+function Restart-Search 
 {
+    Param ( [String[]] $systems )
+
 	$sb = {
-		if ( ( Get-Service -Name OSearch14 | Select -ExpandProperty Status ) -eq "Running" )
-		{
-			Restart-Service -Name OSearch14 -Verbose
+        $search_service = "OSearch14"
+		if ( ( Get-Service -Name $search_service | Select -ExpandProperty Status ) -eq "Running" ) {
+			Restart-Service -Name $search_service -Verbose
 		}
 	}
 	Invoke-Command -ComputerName $systems -ScriptBlock $sb 
 }
 
-function Deploy-Dictionary ( [string] $file )
+function Deploy-Dictionary 
 {
-	$dictionary_file = "custom0009.lex"
-	$dictionary_path = "d$\SharePoint\14\14.0\Bin\"
-
-	DeployTo-SharePointALL -new_file $file -path $dictionary_path -default_file $dictionary_file
-	
-	Restart-Search ( (Get-QueryComponents).QueryComponents | Select -Expand ServerName )
+    Param ( [string] $file )
+	DeployTo-SharePointALL -new_file $file -paths $dictionary_path -default_file $dictionary_file
 }
 
-function Deploy-Thesaurus ( [string] $file )
+function Deploy-Thesaurus
 {
-	$thesaurus_file = "tsenu.xml"
-	$default_thesaurus_path = @("c$\Program Files\Microsoft Office Servers\14.0\Data\Office Server\Config",
-		"c$\Program Files\Microsoft Office Servers\14.0\Data\Config")
-
-	DeployTo-SharePointALL -new_file $file -path $default_thesaurus_path -default_file $thesaurus_file 
+    Param  ( [string] $file )
+	DeployTo-SharePointALL -new_file $file -paths $default_thesaurus_path -default_file $thesaurus_file 
 		
-	$query_components = Get-QueryComponents
-
-	$servers = @()
-	$query_components.QueryComponents | Select ServerName, IndexLocation, Name | % {
-		$org_file =  "\\" + $_.ServerName + "\" + $_.IndexLocation.Replace(":\", "$\") + "\" + $_.Name + "\Config\" + $thesaurus_file 
+	foreach( $query_component in ($query_components.QueryComponents | Select ServerName, IndexLocation, Name ) ) {
+		$org_file =  "\\{0}\{1}\{2}\Config\{3}" -f $query_components.ServerName, $query_components.IndexLocation.Replace(":\", "$\"), $query_components.Name, $thesaurus_file 
 		$backup_file = $org_file + "." + $now
 		
-		copy $org_file $backup_file -verbose
-		copy $file $org_file -Force -verbose
-		
-		$servers += $_.ServerName
+		Copy -Path $org_file -Destination $backup_file -verbose
+		copy -Path $file -Destination $org_file -Force -verbose
 	}
 	
-	Restart-Search $servers
 }
 
-function main()
-{
-	if( -not ( Test-Path $file ) )
-	{
-		Write-Error $file " does not exist"
-		return
-	}
-
-	if( $deploy.ToLower() -eq "dictionary" )
-	{
-		Deploy-Dictionary $file
-	}
-	
-	if( $deploy.ToLower() -eq "thesaurus" )
-	{
-		Deploy-Thesaurus $file
-	}
-}
-main
-	
+$query_components = Get-QueryComponents
+switch ($PsCmdlet.ParameterSetName) { 
+    "Dictionary" { Deploy-Dictionary -file $DictionaryFile }
+    "Thesaurus"  { Deploy-Thesaurus  -file $ThesaurusFile  }
+}	
+Restart-Search ($query_components.QueryComponents | Select -Expand ServerName)
