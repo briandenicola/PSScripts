e59f952bcbd8357395bca629a742ce79ee056d54
Brian Denicola <bjd145@yahoo.com>
2015-08-21 13:19:31 -0500

    New Versions of URL and Web Services Validations

---
+++ ValidationScripts/Url/Validate-WebService.ps1
@@ -0,0 +1,50 @@
+﻿#require -version 4.0
+[CmdletBinding(SupportsShouldProcess=$true)]
+param (
+    [string] $cfg,
+    [switch] $SaveReply
+)
+
+. (Join-path -Path $ENV:SCRIPTS_HOME -ChildPath "Libraries\Standard_Functions.ps1")
+. (Join-path -Path $PWD.Path         -ChildPath "Modules\Helper-Functions.ps1")
+
+Set-Variable -Name hostfile -Value (Join-Path $ENV:SYSTEMROOT "System32\drivers\etc\hosts") -Option Constant
+
+function RemoveFrom-HostFile
+{
+    param(
+        [string] $url
+    )
+
+	((Get-Content $hostfile) -notmatch "^$") -notmatch $url | Out-File -Encoding Ascii $hostfile	
+}
+
+function AddTo-HostFile
+{
+    param(
+        [string] $url,
+        [string] $ip
+    )
+	
+	"`n{0}`t{1}" -f $ip, $url | Out-File -Encoding Ascii -Append -FilePath $hostfile
+}
+
+
+$url_to_validate = Get-Json -Config $cfg
+
+foreach( $url in $url_to_validate ) {
+    $hostname = Select-String -InputObject $url -Pattern 'http?://([\w-]+\.+[\w-]+\.[\w-]+).*'  | Select -Expand Matches | Select -ExpandProperty Groups |  Select -ExpandProperty Value | Select -Last 1
+
+    foreach( $server in ($url.servers | Select -Expand server) ) {
+        
+        $ip = Resolve-DnsName $server | Select -ExpandProperty IpAddress
+        AddTo-HostFile -url $hostname -ip $ip
+        Clear-DNSClientCache 
+
+        $results = Get-GTWebserviceRequest -url $url.url -Server $server -WebService $url.WebService 
+        if( $saveReply ) { Save-Reply -Url $url -Text $results -Server $server }
+        Validate-Results -Rules $url.Rules -Results $results 
+
+        RemoveFrom-HostFile -url $hostname
+    }
+}
