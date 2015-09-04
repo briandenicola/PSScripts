7df9814b72dab91a593198c560c31d949588dc15
Brian Denicola <bjd145@yahoo.com>
2015-09-04 10:55:52 -0500

    chef_with_dsc_with_template

---
+++ DesireStateConfiguration/chef_with_dsc_with_template/metadata.rb
@@ -0,0 +1,7 @@
+name             'chef_with_dsc'
+maintainer       'Brian Denicola'
+maintainer_email 'brian@denicolafamily.com'
+license          'All rights reserved'
+description      'Installs/Configures chef_with_dsc'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.5.5'
