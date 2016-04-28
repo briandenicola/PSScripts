491291362e11626287886373790d160a85dbac6b
Brian Denicola <bjd145@yahoo.com>
2016-04-28 16:36:48 -0500

    Simplified Domain Setup

--- Azure/Create-ADForest-Win2K12.2-For-Testing-In-AzureIaaS.ps1
+++ Azure/Create-ADForest-Win2K12.2-For-Testing-In-AzureIaaS.ps1
@@ -1,42 +1,31 @@
-$ans = Read-Host "This script will install and configure Active Directory Domain Services on this system. Do you want to continue ?"
+param(
+   [string] $DomainName,
+   [string] $RecoveryPassword
+)
 
 #Install Features
 Import-Module ServerManager
 Add-WindowsFeature AD-domain-Services
 
+$SecureRecoveryPassword  = ConvertTo-SecureString $RecoveryPassword -AsPlainText -Force
+
+$opts = @{
+    DomainMode = "Win2012"
+    ForestMode = "Win2012"
+	DomainName = $DomainName
+	DomainNetbiosName = ($DomainName.Split(".")[0])
+	InstallDns = $true 
+    CreateDnsDelegation = $false 
+	NoRebootOnCompletion = $true
+    Force = $true
+	SysvolPath = "C:\AD\SYSVOL"
+    LogPath = "c:\AD\NTDS"
+    DatabasePath = "C:\AD\NTDS"
+    SafeModeAdministratorPassword = $SecureRecoveryPassword 
+}
+
 #Create Domain
 Import-Module ADDSDeployment
-Install-ADDSForest `
-	-CreateDnsDelegation:$false `
-	-DatabasePath "C:\AD\NTDS" `
-	-DomainMode "Win2012" `
-	-DomainName "sharepoint.test" `
-	-DomainNetbiosName "sharepoint" `
-	-ForestMode "Win2012" `
-	-InstallDns:$true `
-	-LogPath "c:\AD\NTDS" `
-	-NoRebootOnCompletion:$false `
-	-SysvolPath "C:\AD\SYSVOL" `
-	-Force:$true
+Install-ADDSForest @opts
 
-#Create Users
-$plain = 'Pa&&word'
-$pass = ConvertTo-SecureString $plain -AsPlainText -Force
-New-ADUser svc_farm -AccountPassword $pass -PasswordNeverExpires $true -Enabled $true -Path "CN=Managed Service Accounts,DC=sharepoint,DC=test"
-New-ADUser svc_search -AccountPassword $pass -PasswordNeverExpires $true -Enabled $true -Path "CN=Managed Service Accounts,DC=sharepoint,DC=test"
-New-ADUser svc_profile -AccountPassword $pass -PasswordNeverExpires $true -Enabled $true -Path "CN=Managed Service Accounts,DC=sharepoint,DC=test"
-New-ADUser svc_web -AccountPassword $pass -PasswordNeverExpires $true -Enabled $true -Path "CN=Managed Service Accounts,DC=sharepoint,DC=test"
-New-ADUser svc_sql -AccountPassword $pass -PasswordNeverExpires $true -Enabled $true -Path "CN=Managed Service Accounts,DC=sharepoint,DC=test"
-
-#Cert Authority
-Add-WindowsFeature Adcs-Cert-Authority -IncludeManagementTools
-Install-AdcsCertificationAuthority -CAType EnterpriseRootCA `
-    -CryptoProviderName "RSA#Microsoft Software Key Storage Provider" `
-    -CACommonName "GTCloud CA" `
-    -KeyLength 2048 `
-    -HashAlgorithmName SHA1 `
-    -ValidityPeriod Years `
-    -ValidityPeriodUnits 15 ` 
-    -CADistinguishedNameSuffix 'dc=sharepoint,dc=test' `
-    -DatabaseDirectory 'C:\Certs\DB' `
-    -LogDirectory 'C:\Certs\Logs'
+Restart-Computer 
