6d87fbc3c28f856d6847e6b0de65fc9db4bea7cc
Brian Denicola <bjdmsdn@outlook.com>
2017-04-28 13:30:40 -0500

    New Script to Audit Resource Creation Times

--- Azure/Audit-AzureResources.ps1
+++ Azure/Audit-AzureResources.ps1
@@ -1,4 +1,13 @@
 <#
+.SYNOPSIS 
+    This script will query an Azure subscription and report back the resources with particular focus on Public IP Addresses 
+
+.DESCRIPTION
+
+.EXAMPLE
+    Audit-AzureResources.ps1 -CSV c:\temp\azure.csv
+
+.NOTES
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
@@ -10,16 +19,6 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-
-.SYNOPSIS 
-    This script will query an Azure subscription and report back the resources with particular focus on Public IP Addresses 
-
-.DESCRIPTION
-
-.EXAMPLE
-    .\Audit-AzureResources.ps1 -CSV c:\temp\azure.csv
-
-.NOTES
 #>
 
 [CmdletBinding()]
