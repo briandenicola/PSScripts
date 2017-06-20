0fdd4d804a1322e4f1d561dd67a176eded947b22
Brian Denicola <bjd145@yahoo.com>
2017-06-20 15:19:40 -0700

    Minor Updates to an old module

--- Libraries/credentials.psm1
+++ Libraries/credentials.psm1
@@ -1,5 +1,6 @@
 function Set-Creds { 
-	$SCRIPT:creds = Get-Credential ( $ENV:USERDOMAIN + "\" + $ENV:USERNAME ) 
+	#$SCRIPT:creds = Get-Credential ( $ENV:USERDOMAIN + "\" + $ENV:USERNAME ) 
+	$SCRIPT:creds = Get-Credential -Message "Please enter your domain password" -UserName ("{0}\{1}" -f $ENV:USERDOMAIN, $ENV:USERNAME )
 }
 
 function Get-Creds {
