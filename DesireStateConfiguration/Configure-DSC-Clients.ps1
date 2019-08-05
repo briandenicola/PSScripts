c7dbe7226b484e98d3d86addc4f2660fcc2cf67f
Brian Denicola <bjd145@yahoo.com>
2019-08-05 10:24:48 -0500

    Fixed file corruption

--- DesireStateConfiguration/Configure-DSC-Clients.ps1
+++ DesireStateConfiguration/Configure-DSC-Clients.ps1
@@ -1,8 +1,26 @@
-﻿param(    [string] $pull_server)configuration Configure_DCSClient {    param ($NodeId, $PullServer)    
-    LocalConfigurationManager    {        AllowModuleOverwrite = 'True'        ConfigurationID = $NodeId        ConfigurationModeFrequencyMins = 30        ConfigurationMode = 'ApplyAndAutoCorrect'        RebootNodeIfNeeded = 'True'        RefreshMode = 'PULL'         DownloadManagerName = 'WebDownloadManager'        DownloadManagerCustomData = (@{ServerUrl = "https://$PullServer/psdscpullserver.svc"})    }}
+param(
+    [string] $pull_server
+)
+
+configuration Configure_DCSClient
+{
+    param ($NodeId, $PullServer)   
+    LocalConfigurationManager
+    {
+        AllowModuleOverwrite = 'True'
+        ConfigurationID = $NodeId
+        ConfigurationModeFrequencyMins = 30
+        ConfigurationMode = 'ApplyAndAutoCorrect'
+        RebootNodeIfNeeded = 'True'
+        RefreshMode = 'PULL'
+        DownloadManagerName = 'WebDownloadManager'
+        DownloadManagerCustomData = (@{ServerUrl = "https://$PullServer/psdscpullserver.svc"})
+    }
+}
 
 $node = [guid]::NewGuid().Guid
-Configure_DCSClient -NodeId $node.ToString() -PullServer $pull_serverSet-DscLocalConfigurationManager -path Configure_DCSClient
+Configure_DCSClient -NodeId $node.ToString() -PullServer $pull_server
+Set-DscLocalConfigurationManager -path Configure_DCSClient
 
 Write-Host "Copy This Guid Down = $($node.ToString())  . . ."
 Get-DscLocalConfigurationManager
