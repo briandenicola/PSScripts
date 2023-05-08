9490c4dbe7b4a8aafc9dcfb514bdc9ab9ad45063
Brian Denicola <bjd145@yahoo.com>
2023-05-08 07:09:39 -0500

    Added notes and examples

--- Miscellaneous/Fix-OneDriveSync.ps1
+++ Miscellaneous/Fix-OneDriveSync.ps1
@@ -1,3 +1,38 @@
+<#
+.SYNOPSIS
+This PowerShell Script will synchronize two directories.  It can copy files from one directory or another.
+
+.DESCRIPTION
+Version - 1.0.0
+The script recurse through a OneDrive directory looking for files not correctly synced locally. It will make a back a backup of the file and rename it to
+kick OneDrive into syncing the file again.
+
+.EXAMPLE
+.\Fix-OneDriveSync.ps1 -Path "D:\OneDrive\Pictures\" -OutputPath \\nas\photo\Backups -Whatif
+
+.EXAMPLE
+.\Fix-OneDriveSync.ps1 -Path "D:\OneDrive\Pictures\" -OutputPath \\nas\photo\Backups -Filter "*.png"
+
+.EXAMPLE
+.\Fix-OneDriveSync.ps1 -Path "D:\OneDrive\Pictures\" -OutputPath \\nas\photo\Backups -StatusOnly
+
+.PARAMETER Path
+Specifies the main directory check for stuck files
+
+.PARAMETER OutputPath
+Specifies the directory to make a backup copy of the stuck files
+
+.PARAMETER Filter
+Filter to only certain files. Default is "*.jpg"
+
+.PARAMETER StatusOnly
+Returns an array of all files currently stuck syncing
+
+.NOTES
+This script requires the OneDriveLib library that can be downloaded from https://github.com/rodneyviana/ODSyncService
+
+#>
+
 [CmdletBinding(SupportsShouldProcess=$true)]
 param(
     [string] $Path,
