eedf364f71ca30c6386b5c496e92c033b15dfd07
briandenicola <brian@briandenicola.com>
2024-03-22 09:31:35 -0500

    ⛸️ Fri Mar 22 09:31:35 CDT 2024

--- Miscellaneous/git-commit.sh
+++ Miscellaneous/git-commit.sh
@@ -2,14 +2,13 @@
 
 # Shameless rip off of https://github.com/benc-uk/dotfiles/blob/master/backup.sh
 
-random_fruit() {
-  fruits=("🍎" "🍊" "🍋" "🍌" "🍉" "🍇" "🍓" "🍒" "🍑" "🍍" "🥝" "🍅" "🍆" "🥑" "🥦" "🥒" "🥬" "🥭" "🥔" "🥕" "🌽" "🌶" "🍎" "🌶️" "🫐" "🥥" "🍄")
-  echo "${fruits[$((RANDOM % ${#fruits[@]}))]}"
+random_sport() {
+  sports=("🏈" "🙏" "⚽" "⚾" "🏀" "🏒" "🥍" "🏐" "🏉" "🏏" "🎱" "🎳" "⛸️" "🥍" "🏓" "🥊" "🎮" "🎲")
+  echo "${sports[$((RANDOM % ${#sports[@]}))]}"
 }
 
 echo -e "\e[34m»»» 📦 \e[32mBacking up repo to GitHub\e[0m"
 
 git add .
-git commit -m "$(random_fruit) $(date)"
+git commit -m "$(random_sport) $(date)"
 git push
-
